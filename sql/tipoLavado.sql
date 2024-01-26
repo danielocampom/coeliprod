a402bfad0b11f7923bd5ea19bf8171b41dc0155f
Daniel Ocampo <carlosdanielocampo62@gmail.com>
2024-01-26 09:45:00 -0600

    Cambio de reporte

--- sql/tipoLavado.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tipo_lavado(id_tipo_lavado,fecha_registro, nomre_tpl, usuario_id_usuario) VALUES (1,GETDATE(),'LAVADO EN SECO',1);
-INSERT INTO tipo_lavado(id_tipo_lavado,fecha_registro, nomre_tpl, usuario_id_usuario) VALUES (2,GETDATE(),'LAVADO EN HUMEDO CA',1);
-INSERT INTO tipo_lavado(id_tipo_lavado,fecha_registro, nomre_tpl, usuario_id_usuario) VALUES (3,GETDATE(),'SECADO EN HUMEDO CA',1);
-INSERT INTO tipo_lavado(id_tipo_lavado,fecha_registro, nomre_tpl, usuario_id_usuario) VALUES (4,GETDATE(),'LAVADO EN HUMEDO CL',1);
-INSERT INTO tipo_lavado(id_tipo_lavado,fecha_registro, nomre_tpl, usuario_id_usuario) VALUES (5,GETDATE(),'SECADO EN HUMEDO CL',1);
